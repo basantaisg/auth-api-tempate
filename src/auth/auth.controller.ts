7de7e664f95d464c331002183d75af904672318e
Basanta <basantaparajuli.11@gmail.com>
2025-08-15 16:27:11 +0545

    final code

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,4 +1,53 @@
-import { Controller } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Get,
+  Post,
+  Req,
+  Res,
+  UseGuards,
+} from '@nestjs/common';
+import { AuthService } from './auth.service';
+import { RegisterDto } from './dtos/register.dto';
+import { LoginDto } from './dtos/login.dto';
+import type { Response } from 'express';
+import { AuthGuard } from '@nestjs/passport';
 
-@Controller('auth')
-export class AuthController {}
+@Controller('api/auth')
+export class AuthController {
+  constructor(private readonly authService: AuthService) {}
+
+  @Post('signup')
+  async register(@Body() registerDto: RegisterDto): Promise<any> {
+    return await this.authService.register(registerDto);
+  }
+
+  @Post('login')
+  async login(
+    @Body() loginDto: LoginDto,
+    @Res({ passthrough: true }) res: Response,
+  ): Promise<{}> {
+    return await this.authService.login(loginDto, res);
+  }
+
+  @UseGuards(AuthGuard('jwt-refresh'))
+  @Post('refresh')
+  refresh(@Req() req, @Res({ passthrough: true }) res: Response) {
+    return this.authService.refreshAccessToken(req.user, res);
+  }
+
+  @Post('logout')
+  logout(@Res({ passthrough: true }) res: Response) {
+    res.clearCookie('refresh_token', {
+      httpOnly: true,
+      secure: true,
+      sameSite: 'strict',
+    });
+  }
+
+  @UseGuards(AuthGuard('jwt'))
+  @Get('profile')
+  getProfile(@Req() req) {
+    return req.user;
+  }
+}
--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,6 +1,10 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, UnauthorizedException } from '@nestjs/common';
 import { JwtService } from '@nestjs/jwt';
 import { PrismaService } from 'src/prisma/prisma.service';
+import { RegisterDto } from './dtos/register.dto';
+import bcrypt from 'node_modules/bcryptjs';
+import { LoginDto } from './dtos/login.dto';
+import { Response } from 'express';
 
 @Injectable()
 export class AuthService {
@@ -9,5 +13,60 @@ export class AuthService {
     private readonly jwtService: JwtService,
   ) {}
 
-  async register() {}
+  async register(registerDto: RegisterDto): Promise<any> {
+    const hashedPassword = await bcrypt.hash(registerDto.password, 10);
+    const user = await this.prisma.user.create({
+      data: { ...registerDto, password: hashedPassword },
+    });
+
+    return { id: user.id, username: user.email, email: user.email };
+  }
+
+  async login(loginDto: LoginDto, res: Response): Promise<{}> {
+    const { email, password } = loginDto;
+    const user = await this.prisma.user.findUnique({
+      where: { email },
+    });
+
+    if (!user) throw new UnauthorizedException('Invalid Email!');
+
+    const passwordMatches = await bcrypt.compare(password, user.password);
+    if (!passwordMatches)
+      throw new UnauthorizedException("Password doesn't match!");
+
+    // Generation of token starts here...
+    const payload = { sub: user.id, email: user.email };
+
+    // access token!
+    const accessToken = this.jwtService.sign(payload, {
+      secret: process.env.JWT_ACCESS_SECRET,
+      expiresIn: '15m',
+    });
+
+    // const refresh token!
+    const refreshToken = this.jwtService.sign(payload, {
+      secret: process.env.JWT_REFRESH_SECRET,
+      expiresIn: '7d',
+    });
+
+    // sending into cookies!
+
+    res.cookie('refresh_token', refreshToken, {
+      httpOnly: true,
+      secure: true,
+      sameSite: 'strict',
+      maxAge: 7 * 24 * 60 * 60 * 1000,
+    });
+
+    return { accessToken };
+  }
+
+  async refreshAccessToken(user: any, res: Response) {
+    const payload = { sub: user.id, email: user.email };
+    const accessToken = this.jwtService.sign(payload, {
+      secret: process.env.JWT_ACCESS_SECRET,
+      expiresIn: '15m',
+    });
+    return { accessToken };
+  }
 }
